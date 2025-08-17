6f55ac06ab5575698fec1c336b13d649e8e370e6
Krish-dua <krishdua747@gmail.com>
2025-08-18 00:14:38 +0530

    again a typo fix

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
 })
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(_dirname,'/frontend/dist')))
+app.use(express.static(path.join(_dirname,'/Frontend/dist')))
 
 
 app.use("/api",urlRoutes)
@@ -21,7 +21,7 @@ app.get('/:shortenid',redirect)
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(_dirname, 'frontend/dist/index.html'))
+    res.sendFile(path.join(_dirname, 'Frontend/dist/index.html'))
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
